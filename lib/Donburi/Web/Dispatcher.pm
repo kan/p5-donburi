8901701af17400e4d22b2964901012d73a7cc3a7
Nishibayashi Takuji <takuji@senchan.jp>
2011-06-23 16:16:22 +0900

    JSONRPCを実装

--- lib/Donburi/Web/Dispatcher.pm
+++ lib/Donburi/Web/Dispatcher.pm
@@ -10,6 +10,7 @@ sub new {
     my $class = shift;
 
     my $router = Router::Simple->new();
+    $router->connect('/', { controller => 'JSONRPC', action => 'call' }, {on_match => \&is_called_json_rpc});
     $router->connect('/', { controller => 'Root', action => 'index' });
     $router->connect('/post', { controller => 'Root', action => 'post' });
     $router->connect('/channel/', { controller => 'Channel', action => 'index' });
@@ -21,7 +22,7 @@ sub new {
 
 sub dispatch {
     my ($self, $env) = @_;
-    
+
     if ( my $p = $self->{router}->match($env) ) {
         my $c = "Donburi::Web::C::" . $p->{controller};
         my $action = 'do_' . $p->{action};
@@ -33,6 +34,12 @@ sub dispatch {
     } else {
         return [ 404, [], ['not found'] ];
     }
+}
+
+sub is_called_json_rpc {
+    my ($env, $match) = @_;
+    my $req = Plack::Request->new($env);
+    return defined $req->param('params') && defined $req->param('method');
 }
 
 1;
